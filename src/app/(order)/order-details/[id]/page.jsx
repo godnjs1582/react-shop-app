6e48fbd802f9ebfdb171aedc54e06d287ba237d1
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2023-08-03 17:26:58 +0900

    FEAT:폴더 구조 생성

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderDetails = ({params, searchParams}) => {
+  const {id}=params;
+  const {hello}=searchParams
+  return (
+    <div>{id}{hello}
+    </div>
+  )
+}
+
+export default OrderDetails
